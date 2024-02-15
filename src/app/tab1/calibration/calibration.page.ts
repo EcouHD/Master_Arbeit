b82dd6ab1cc430cafe64b5843cb3bc34426c5110
Lars Eckel <eckellars@gmail.com>
2024-02-15 12:50:42 +0100

    refactor: change of grid to hide calibrationButton until the end

--- src/app/tab1/calibration/calibration.page.ts
+++ src/app/tab1/calibration/calibration.page.ts
@@ -1,19 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
 declare var webgazer: any;
 
-const BUTTONS = [
-  { id: 1, clicked: 0 },
-  { id: 2, clicked: 0 },
-  { id: 3, clicked: 0 },
-  { id: 4, clicked: 0 },
-  { id: 5, clicked: 0 },
-  { id: 6, clicked: 0 },
-  { id: 7, clicked: 0 },
-  { id: 8, clicked: 0 },
-  { id: 9, clicked: 0 }
-]
+const BUTTONS = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 @Component({
   selector: 'app-calibration',
@@ -26,15 +16,7 @@ export class CalibrationPage implements OnInit {
 
   buttons = BUTTONS;
 
-  @ViewChild('Pt0') button0: any;
-  @ViewChild('Pt1') button1: any;
-  @ViewChild('Pt2') button2: any;
-  @ViewChild('Pt3') button3: any;
-  @ViewChild('Pt4') button4: any;
-  @ViewChild('Pt5') button5: any;
-  @ViewChild('Pt6') button6: any;
-  @ViewChild('Pt7') button7: any;
-  @ViewChild('CalibrationPoint') buttonCalibration: any;
+  @ViewChildren('dot') dots!: QueryList<ElementRef>;
 
   pointsCalibrated = 0;
 
@@ -43,112 +25,45 @@ export class CalibrationPage implements OnInit {
   constructor(navController: NavController) {
     this.navController = navController
     webgazer.resume();
-    this.buttons.forEach(element => { element.clicked = 0});
+    this.buttons.forEach(element => { element = 0 });
   }
 
   ngOnInit() {
   }
 
 
-  buttonClicked(buttonId: any) {
-    if (buttonId == 0) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button0.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button0.el.style.setProperty('--background', 'red')
-        this.button0.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 1) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button1.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button1.el.style.setProperty('--background', 'red')
-        this.button1.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 2) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button2.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button2.el.style.setProperty('--background', 'red')
-        this.button2.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 3) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button3.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button3.el.style.setProperty('--background', 'red')
-        this.button3.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 4) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button4.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button4.el.style.setProperty('--background', 'red')
-        this.button4.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 5) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button5.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button5.el.style.setProperty('--background', 'red')
-        this.button5.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 6) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button6.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button6.el.style.setProperty('--background', 'red')
-        this.button6.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 7) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.button7.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5) {
-        this.button7.el.style.setProperty('--background', 'red')
-        this.button7.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    } else if (buttonId == 8) {
-      this.buttons[buttonId].clicked++;
-      if (this.buttons[buttonId].clicked < 5) {
-        var opacity = 1 - 0.2*this.buttons[buttonId].clicked + 0.1
-        this.buttonCalibration.el.style.setProperty('--opacity', opacity)
-      } else if (this.buttons[buttonId].clicked == 5){
-        this.buttonCalibration.el.style.setProperty('--background', 'red')
-        this.buttonCalibration.el.style.setProperty('--opacity', '1.0')
-        this.pointsCalibrated++
-      }
-    }
+  buttonClicked(index: any) {
+    const dot = this.dots.find((x: ElementRef, i: number) => i == index);
 
-    if(this.pointsCalibrated == 8) {
+    if (this.buttons[index] == 0) {
+      dot?.nativeElement.classList.remove('state1');
+      dot?.nativeElement.classList.add('state2');
+    } else if (this.buttons[index] == 1) {
+      dot?.nativeElement.classList.remove('state2');
+      dot?.nativeElement.classList.add('state3');
+    } else if (this.buttons[index] == 2) {
+      dot?.nativeElement.classList.remove('state3');
+      dot?.nativeElement.classList.add('state4');
+    } else if (this.buttons[index] == 3) {
+      dot?.nativeElement.classList.remove('state4');
+      dot?.nativeElement.classList.add('state5');
+    } else if (this.buttons[index] == 4) {
+      dot?.nativeElement.classList.remove('state5');
+      dot?.nativeElement.classList.add('stateFinished');
+      this.pointsCalibrated++;
+    }
+    this.buttons[index]++;
+    console.log(this.pointsCalibrated);
+
+    if (this.pointsCalibrated == 8 && this.calibrationButtonDisable) {
+      //activate middle button
+      const middleDot = this.dots.find((x: ElementRef, i: number) => i == 4);
+      middleDot?.nativeElement.classList.remove('calibration');
+      middleDot?.nativeElement.classList.add('state1');
       this.calibrationButtonDisable = false;
-      this.buttonCalibration.el.style.setProperty('--opacity', '1.0')
     }
 
-    if(this.pointsCalibrated >=9) {
+    if (this.pointsCalibrated >= 9) {
       console.log("Calibration finished. Start accuracy calc!")
     }
   }
